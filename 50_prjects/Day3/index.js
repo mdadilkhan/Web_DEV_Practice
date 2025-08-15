88b56469e67fb887d62bfdc19e7b6728acf011aa
mdadilkhan <mdadilakhtar8@gmail.com>
2025-08-15 21:06:27 +0530

    Amazing Article

---
+++ 50_prjects/Day3/index.js
@@ -0,0 +1,19 @@
+const open =document.getElementById('open')
+const close =document.getElementById('close')
+
+const container=document.querySelector('.container')
+
+
+console.log(open);
+console.log(close);
+console.log(container);
+
+
+
+open.addEventListener('click',()=>{
+    container.classList.add('show-nav')
+})
+
+close.addEventListener('click',()=>{
+    container.classList.remove('show-nav')
+})
